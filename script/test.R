62f3d9f4f52eb368eb3730d1c78660a08d06f412
andreaspano <andrea.spano@quantide.com>
2019-04-16 23:27:03 +0200

    works on dumpled rds

--- script/test.R
+++ script/test.R
@@ -4,48 +4,25 @@ require(ggplot2)
 require(RColorBrewer)
 
 
-ii <- load_clan(tag = "#282UJRU28", auth_key = token('~/dev/rcoc/tokens/token001'))
-
-
-f <- function(tag, auth_key ){
-
-  url <- paste( "https://api.clashofclans.com/v1/clans",  repash(tag), sep= '/')
-
-  r <- GET(url, add_headers(Authorization = paste("Bearer", auth_key, sep = " ")))
-
-  r <- content(r, as = "parsed") 
-
-  return(r)
-}
-
-i <- load_clan( tag = "#282UJRU28", token('~/dev/rcoc/tokens/token001'))
+#ii <- load_clan(tag = "#282UJRU28", auth_key = token('~/dev/rcoc/tokens/token001'))
+clan <- readRDS('./clan.Rds')
 
 
 
-#clantag <- "#282UJRU28"
-clantag <- "%23282UJRU28"
-
-token <- "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNlZWM5NTBhLTUzZTEtNGYxOS05NmU5LTc0NTRhMGJjNmZkNCIsImlhdCI6MTU1NTI1OTQ3MSwic3ViIjoiZGV2ZWxvcGVyLzkwOWIwNGFhLTAzZDItMTljMS1kNDA0LTgzNjBlNzg4MTQ0MSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjkzLjcxLjguOTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.pqLcpAfhLhzVH-gajztu7Q8RPXQqKZiguRyyLuLcEmXzT1C7kUW5Zfpzbc7U2uXLDSVEKcCjXQCuCS3hS8TuAg"
 
 
-url <- paste( "https://api.clashofclans.com/v1/clans",  clantag, sep= '/')
-
-#url <- paste(url, 'currentwar', sep = '/')
-r <- GET(url, add_headers(Authorization = paste("Bearer", token, sep = " ")))
-stop_for_status(r)
-#content(r)
-r<-content(r,as="parsed") 
 
 
-d <- tibble(name = vapply(r$memberList, '[[', 'name', FUN.VALUE = character(1)), 
-            trophies = vapply(r$memberList, '[[', 'trophies', FUN.VALUE = numeric(1)))
+
+d <- tibble(name = vapply(clan$memberList, '[[', 'name', FUN.VALUE = character(1)),
+            trophies = vapply(clan$memberList, '[[', 'trophies', FUN.VALUE = numeric(1)))
 
 
 png('~/tmp/smile.png')
-ggplot(d) + geom_bar (aes(reorder(name, trophies) , trophies), fill = 'lightblue', stat = 'identity') + 
-    coord_flip() + 
+ggplot(d) + geom_bar (aes(reorder(name, trophies) , trophies), fill = 'lightblue', stat = 'identity') +
+    coord_flip() +
     theme(legend.position = "none") +
-    xlab ('Number of trophies') + 
+    xlab ('Number of trophies') +
     ylab('Name') +
     scale_fill_manual(values = heat.colors(nrow(d), alpha=1))
 dev.off()
@@ -61,7 +38,7 @@ url <- paste( "https://api.clashofclans.com/v1/clans",  clantag, sep= '/')
 url <- paste(url, 'currentwar', sep = '/')
 r <- GET(url, add_headers(Authorization = paste("Bearer", token, sep = " ")))
 stop_for_status(r)
-r<-content(r,as="parsed") 
+r<-content(r,as="parsed")
 
 team_size <- r$teamSize
 
@@ -75,14 +52,14 @@ number_of_attack <- function(r) {
 
 
     team_size <- r$teamSize
-    lapply(seq_len(team_size) , .number_of_attack, r = r) 
+    lapply(seq_len(team_size) , .number_of_attack, r = r)
 
 }
 
 
 number_of_attack(r)
 
-d <- tibble(name = vapply(r$memberList, '[[', 'name', FUN.VALUE = character(1)), 
+d <- tibble(name = vapply(r$memberList, '[[', 'name', FUN.VALUE = character(1)),
             trophies = vapply(r$memberList, '[[', 'trophies', FUN.VALUE = numeric(1)))
 
 
