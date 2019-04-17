b75ee5ff163c24a352e059fda63036129fae5e22
andreaspano <andrea.spano@quantide.com>
2019-04-17 23:02:59 +0200

    dev script

--- script/test.R
+++ script/test.R
@@ -1,18 +1,74 @@
-require(httr)
-require(dplyr)
-require(ggplot2)
-require(RColorBrewer)
+rm(list =  ls())
+require(rcoc)
+# require(httr)
+# require(dplyr)
+# require(ggplot2)
+# require(RColorBrewer)
 
+#' load_clan( tag = "#282UJRU28", token('~/dev/rcoc/tokens/token001'))
 
 #ii <- load_clan(tag = "#282UJRU28", auth_key = token('~/dev/rcoc/tokens/token001'))
+
 clan <- readRDS('./clan.Rds')
+class(clan) <-  'clan'
+this_clan <-  info(clan)
+this_member <-  member(clan)
 
 
 
 
 
+#####################################
+
+x <- this_member$trophies
+n <- length(x)
+score <-  seq_len(n)/n
+plot( x, rev(score), type = 'l')
+abline(v = median(x))
+abline(h = .5)
+
+#####################################
+
+names(clan)
+
+#"badgeUrls"
+map_clan_info <-  list (
+
+  elem = c(
+  "tag","name","type","location","clanLevel",
+  "clanPoints","clanVersusPoints","requiredTrophies","warFrequency",
+  "warWinStreak","warWins","warTies","warLosses","isWarLogPublic"
+  ),
+
+  lang = list(
+    eng = c(
+      "tag","name","type","description","location","clan level",
+      "clan points","clan versus points","required trophies","war frequency",
+      "war win streak","war wins","war ties","war losses","is war log public"
+    )))
+
+lang <- function(map, .lang) {
+  map$lang[[.lang]]
+}
+
+elem <- function(map) map$elem
+
+map <- map_clan_info
+
+fun_clan_info <- function(map) {
+
+  tibble(lang(clan_info, 'eng'),
+         unlist(clan[elem(clan_info)])
+  )
+
+}
+
+fun_clan_info()
 
 
+
+
+clan[clan_info]
 
 d <- tibble(name = vapply(clan$memberList, '[[', 'name', FUN.VALUE = character(1)),
             trophies = vapply(clan$memberList, '[[', 'trophies', FUN.VALUE = numeric(1)))
