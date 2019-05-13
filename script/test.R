97beb54849b11e7c685197b99bf4efca013a8590
andreaspano <andrea.spano@quantide.com>
2019-05-13 21:12:23 +0000

    boh

--- script/test.R
+++ script/test.R
@@ -1,133 +1,75 @@
 rm(list =  ls())
 require(rcoc)
- require(httr)
- require(dplyr)
+require(httr)
+require(dplyr)
 require(lubridate)
 require(readr)
-# require(ggplot2)
-# require(RColorBrewer)
-
-#' load_clan( tag = "#282UJRU28", token('~/dev/rcoc/tokens/token001'))
-clan <- load_clan(tag = "#282UJRU28", auth_key = token('~/dev/rcoc/tokens/token-34'))
-export(clan , path =  './export')
+require(ggplot2)
 
 
+# Load  my clan
+my_clan <- load_clan(tag = "#282UJRU28", auth_key = token('~/dev/rcoc/tokens/token-34'))
+#  info about my clan
+my_clan_info <-  info(clan)
+# table of my clan members
+my_clan_member <-  member(my_clan)
+my_clan_member %>% select(name,  tag)
 
-# Clan di sergio
-#clan <- load_clan(tag = "#QYCUJPVL", auth_key = token('~/dev/rcoc/tokens/token-34'))
 
-# tag <- "#282UJRU28"
-
-
-
-
-  #clan <- readRDS('./clan.Rds')
-#class(clan) <-  'clan'
-this_clan <-  info(clan)
-this_member <-  member(clan)
+# Load my friend clan
+friend_clan <- load_clan(tag = "#QYCUJPVL", auth_key = token('~/dev/rcoc/tokens/token-34'))
+# info about my fiend clan
+friend_clan_info <-  info(friend_clan)
+# table of my clan members
+friend_clan_member <-  member(friend_clan)
 
 
 
+# Load current war
+this_war <- load_war(tag = "#282UJRU28", auth_key = token('~/dev/rcoc/tokens/token-34'))
+#write_rds(this_war, './export/war_20190420.rds')
 
-#####################################
 
-x <- this_member$trophies
-n <- length(x)
-score <-  seq_len(n)/n
-plot( x, rev(score), type = 'l')
-abline(v = median(x))
-abline(h = .5)
 
-#####################################
+# see attacks during war
+war_attack <- get_attack(war = this_war)
 
-names(clan)
-
-#"badgeUrls"
-map_clan_info <-  list (
-
-  elem = c(
-  "tag","name","type","location","clanLevel",
-  "clanPoints","clanVersusPoints","requiredTrophies","warFrequency",
-  "warWinStreak","warWins","warTies","warLosses","isWarLogPublic"
-  ),
-
-  lang = list(
-    eng = c(
-      "tag","name","type","description","location","clan level",
-      "clan points","clan versus points","required trophies","war frequency",
-      "war win streak","war wins","war ties","war losses","is war log public"
-    )))
-
-lang <- function(map, .lang) {
-  map$lang[[.lang]]
-}
-
-elem <- function(map) map$elem
-
-map <- map_clan_info
-
-fun_clan_info <- function(map) {
-
-  tibble(lang(clan_info, 'eng'),
-         unlist(clan[elem(clan_info)])
-  )
-
-}
-
-fun_clan_info()
+ggplot(war_attack) +
+  geom_line(aes(order, cum_win, group =  attacker_clan_name, color = attacker_clan_name)) +
+  geom_line(aes(order, cum_star, group =  attacker_clan_name, color = attacker_clan_name)) +
+  geom_line(aes(order, cum_attack, group =  attacker_clan_name,color = attacker_clan_name)) +
+  geom_ribbon(aes(x =  order, ymin = cum_win, ymax = cum_star, group =  attacker_clan_name, fill = attacker_clan_name), alpha =  0.5) +
+  xlab ('Attack Order') +ylab('Results') +
+  theme_minimal() +
+  theme(legend.title=element_blank())
 
 
 
 
-clan[clan_info]
+# gather info  about opponent clan
+opponent <- this_war$opponent$tag %>% load_clan(auth_key = token('~/dev/rcoc/tokens/token-34'))
+opponent_clan <-  info(opponent)
+opponent_member <-  member(opponent)
 
-d <- tibble(name = vapply(clan$memberList, '[[', 'name', FUN.VALUE = character(1)),
-            trophies = vapply(clan$memberList, '[[', 'trophies', FUN.VALUE = numeric(1)))
-
-
-png('~/tmp/smile.png')
-ggplot(d) + geom_bar (aes(reorder(name, trophies) , trophies), fill = 'lightblue', stat = 'identity') +
-    coord_flip() +
-    theme(legend.position = "none") +
-    xlab ('Number of trophies') +
-    ylab('Name') +
-    scale_fill_manual(values = heat.colors(nrow(d), alpha=1))
-dev.off()
+# compare exp points between clan and opponent
+compare(war = this_war , max_lag = 3)
 
 
-# https://www.whatismyip.com/ to know your IP address
-#home_ip_address <- "93.71.8.96"
+# checjk single player
+#my_clan_member %>% select (name, tag) #PQYGGRVUR
 
+this_player <- load_player( tag = "#PQYGGRVUR", token('~/dev/rcoc/tokens/token-34'))
+names(this_player)
+bind_rows(this_player$troops)
+bind_rows(this_player$spells)
+bind_rows(this_player$heroes)
 
-#############################################################################################
+alex <- load_player( tag = "#QPPVPU8Q", token('~/dev/rcoc/tokens/token-34'))
 
-url <- paste( "https://api.clashofclans.com/v1/clans",  clantag, sep= '/')
-url <- paste(url, 'currentwar', sep = '/')
-r <- GET(url, add_headers(Authorization = paste("Bearer", token, sep = " ")))
-stop_for_status(r)
-r<-content(r,as="parsed")
+#QPPVPU8Q
+bind_rows(alex$troops)
 
-team_size <- r$teamSize
-
-
-.number_of_attack <- function(i, r) {
-    r$clan$members[[i]]$attacks
-}
-
-
-number_of_attack <- function(r) {
-
-
-    team_size <- r$teamSize
-    lapply(seq_len(team_size) , .number_of_attack, r = r)
-
-}
-
-
-number_of_attack(r)
-
-d <- tibble(name = vapply(r$memberList, '[[', 'name', FUN.VALUE = character(1)),
-            trophies = vapply(r$memberList, '[[', 'trophies', FUN.VALUE = numeric(1)))
+str(this_player)
 
 
 
