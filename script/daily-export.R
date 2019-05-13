3ab27178dff2c41bfbab948a79cdb43069650a89
andreaspano <andrea.spano@quantide.com>
2019-05-13 21:12:11 +0000

    changed token

--- script/daily-export.R
+++ script/daily-export.R
@@ -1,5 +1,6 @@
 #!/usr/bin/Rscript
 require(rcoc)
+require(rmarkdown)
+
 clan <- load_clan(tag = "#282UJRU28", auth_key = token('/home/andrea/dev/rcoc/tokens/token-34'))
 export(clan , path =  '/home/andrea/dev/rcoc/export')
-cat('game over', '\n')
